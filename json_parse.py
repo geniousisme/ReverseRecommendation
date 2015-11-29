367242eb0ce41890ee12caf1fa9f2ca224ce542d
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-29 17:02:45 -0500

    Modify the json parser a little

--- json_parse.py
+++ json_parse.py
@@ -2,3 +2,8 @@ import json
 
 file = open("yelp_academic_dataset_review.json")
 reviews = [json.loads(line) for line in file]
+
+negative_reviews = []
+for review in reviews:
+    if review[u'stars'] <= 3:
+        negative_reviews.append(review)
